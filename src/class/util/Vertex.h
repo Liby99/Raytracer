a23d0a94b409985f041826244986fe24f7c6d4fa
Liby99 <liby99@icloud.com>
2017-04-08 00:05:06 -0700

    Done Vertex

--- src/class/util/Vertex.h
+++ src/class/util/Vertex.h
@@ -1,17 +1,25 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include "util/Transform.h"
+
 class Vertex {
 private:
+    
+    // Basic parameter for vertex
     vec3 position;
     vec3 normal;
 public:
     
-    //
+    // Constructor
+    Vertex(vec3 position);
     Vertex(vec3 position, vec3 normal);
     
-    //
+    // Basic getter and setter
     vec3 getPosition();
-    vec3 getNormal();
-    
-    //
     void setPosition(vec3 position);
+    vec3 getNormal();
     void setNormal(vec3 normal);
-}
+};
+
+#endif
---
+++ src/private/util/Vertex.cpp
@@ -0,0 +1,27 @@
+#include "util/Vertex.h"
+
+Vertex::Vertex(vec3 position) {
+    setPosition(position);
+    setNormal(vec3());
+}
+
+Vertex::Vertex(vec3 position, vec3 normal) {
+    setPosition(position);
+    setNormal(normal);
+}
+
+vec3 Vertex::getPosition() {
+    return position;
+}
+
+void Vertex::setPosition(vec3 position) {
+    this->position = position;
+}
+
+vec3 Vertex::getNormal() {
+    return normal;
+}
+
+void Vertex::setNormal(vec3 normal) {
+    this->normal = normal;
+}
