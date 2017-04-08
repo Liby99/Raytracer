95439c9c75c20ebd055263247753aae365f4f566
Liby99 <liby99@icloud.com>
2017-04-08 10:09:41 -0700

    Done Project 1 Scene 1

--- src/p1s1.cpp
+++ src/p1s1.cpp
@@ -1,8 +1,17 @@
+#include "scene/Scene.h"
+#include "material/Diffuse.h"
+#include "light/PointLight.h"
+#include "light/DirectionalLight.h"
+#include "object/Cube.h"
+#include "camera/Camera.h"
+#include "util/Bitmap.h"
+#include "util/Color.h"
+
 int main(int argc, char ** argv) {
     
     // Setup scene
     Scene scene;
-    scene.setBackgroundColor(Color(0.5, 0.5, 0.5));
+    scene.setBackgroundColor(Color(0.8, 0.9, 1));
     
     // Setup basic material
     Diffuse diffuse = Diffuse(Color(1, 1, 1));
@@ -22,9 +31,17 @@ int main(int argc, char ** argv) {
     floor.translateY(-0.5);
     scene.addObject(floor);
     
+    DirectionalLight sun = DirectionalLight(Color(1, 1, 0.9), vec3(-0.5, -1, -0.5));
+    sun.setIntensity(0.5);
+    
+    PointLight point = PointLight(Color(1, 0.2, 0.2), vec3(2, 2, 0));
+    point.setIntensity(0.5);
+    
     Camera camera;
     camera.lookAt(vec3(3, 3, 3), vec3(0, 0, 0), vec3(0, 1, 0));
+    camera.setSize(800, 600);
+    camera.setFovy(90);
     
     Bitmap bmp = camera.render(scene);
-    bmp.sameImage("project1.bmp");
+    bmp.saveImage("project1.bmp");
 }
