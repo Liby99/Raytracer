95d5cb940ea468e1fb2d47d7652ac7855cc77d4e
Liby99 <liby99@icloud.com>
2017-04-24 12:15:09 -0700

    Add Project2 Tests

--- test/p2t1.cpp
+++ test/p2t1.cpp
@@ -1,5 +1,5 @@
 #include "scene/Scene.h"
-#include "object/Cube.h"
+#include "object/Plane.h"
 #include "object/BoxTreeObject.h"
 #include "object/InstanceObject.h"
 #include "material/Diffuse.h"
@@ -8,7 +8,6 @@
 #include "camera/Camera.h"
 #include "util/Bitmap.h"
 #include "util/Color.h"
-#include "module/StopWatch.h"
 
 #include <iostream>
 
@@ -23,21 +22,29 @@ int main() {
     Diffuse diffuse = Diffuse();
     
     // Create ground
-    Cube ground = Cube(5, 0.1, 5);
+    Plane ground = Plane();
+    ground.translateY(-0.1);
     ground.addMaterial(diffuse);
     scn.addObject(ground);
     
     // Create dragon
-    BoxTreeObject dragon = BoxTreeObject("res/dragon.ply");
+    BoxTreeObject dragon = BoxTreeObject();
+    for (int i = 0; i < 10; i++) {
+        dragon.addVertex(vec3(0, 0, -0.25 + i * 0.05));
+        dragon.addVertex(vec3(0, 0.1, -0.25 + i * 0.05));
+        dragon.addVertex(vec3(0.1, 0, -0.25 + i * 0.05));
+        dragon.addTriangle(3 * i + 2, 3 * i + 1, 3 * i);
+        if (i >= 1) dragon.addTriangle(3 * i, 3 * i + 1, 3 * i - 2);
+    }
     dragon.addMaterial(diffuse);
     scn.addObject(dragon);
     
-    // Create instance
-    InstanceObject inst = InstanceObject(dragon);
-    inst.translate(vec3(-0.05f, 0.0f, -0.1f));
-    inst.rotateY(180);
-    inst.addMaterial(diffuse);
-    scn.addObject(inst);
+    // // Create instance
+    // InstanceObject inst = InstanceObject(dragon);
+    // inst.translate(vec3(-0.05f, 0.0f, -0.1f));
+    // inst.rotateY(180);
+    // inst.addMaterial(diffuse);
+    // scn.addObject(inst);
     
     // Create lights
     DirectionalLight sunlgt = DirectionalLight(Color(1.0f, 1.0f, 0.9f), vec3(2.0f, -3.0f, -2.0f));
@@ -56,18 +63,11 @@ int main() {
     
     // Create camera
     Camera cam;
-    cam.lookAt(vec3(-0.1f, 0.1f, 0.2f), vec3(-0.05f, 0.12f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+    cam.lookAt(vec3(0.3f, 0.2f, 0.5f), vec3(0, 0, 0), vec3(0, 1.0f, 0));
     cam.setFovy(40.0f);
     cam.setResolution(800, 600);
     
-    cout << "Starting to count time" << endl;
-    StopWatch watch;
-    watch.start();
-    
     // Render image
     Bitmap bmp = cam.render(scn);
-    bmp.saveImage("project2.bmp");
-    
-    cout << "Total Runtime: " << watch.stop() << "s" << endl;
+    bmp.saveImage("project2test1.bmp");
 }
- 
