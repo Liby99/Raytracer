a2668e2021a935d13f0110519c721188682cec9b
Liby99 <liby99@icloud.com>
2017-05-30 21:42:15 -0700

    Done AreaLight. Again

--- source/extern/light/Light.h
+++ source/extern/light/Light.h
@@ -31,12 +31,12 @@ public:
     Light(Color c);
     
     // Color related setter getter
-    Color getColor();
-    void setColor(Color color);
-    float getIntensity();
-    void setIntensity(float intensity);
-    bool getCastShadow();
-    void setCastShadow(bool castShadow);
+    virtual Color getColor();
+    virtual void setColor(Color color);
+    virtual float getIntensity();
+    virtual void setIntensity(float intensity);
+    virtual bool getCastShadow();
+    virtual void setCastShadow(bool castShadow);
     
     // Illuminate
     virtual float getBrightness(Scene & scene, Intersection & intersection, float t);
