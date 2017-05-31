72c8fe1a419bf2a6013fa4d50ee917d313979188
Liby99 <liby99@icloud.com>
2017-05-30 17:58:47 -0700

    Combined BoxTreeNode and Object File

--- source/extern/object/BoxTreeObject.h
+++ source/extern/object/BoxTreeObject.h
@@ -2,7 +2,40 @@
 #define BOX_TREE_OBJECT_H
 
 #include "object/MeshObject.h"
-#include "object/BoxTreeNode.h"
+#include "object/Triangle.h"
+
+class BoxTreeNode {
+protected:
+    
+    // Left and right nodes if not leaf
+    BoxTreeNode * left;
+    BoxTreeNode * right;
+    
+    // Variables
+    vector<Triangle *> triangles;
+    BoundingBox box;
+    bool leafFlag;
+    
+    // Protected constructor, only used for internal call
+    BoxTreeNode(vector<Triangle *> & tris, int start, int amount);
+    
+public:
+    
+    // Constructor
+    BoxTreeNode(Triangle * tris);
+    BoxTreeNode(vector<Triangle *> & tris);
+    ~BoxTreeNode();
+    
+    // Getter
+    BoxTreeNode & getLeft();
+    BoxTreeNode & getRight();
+    BoundingBox & getBoundingBox();
+    
+    // Helper Methods
+    bool leftRightIntersect();
+    bool isLeaf();
+    bool intersect(Ray & ray, Intersection & intersection);
+};
 
 class BoxTreeObject : public MeshObject {
 protected:
